5d9a17b6a19a07f204b9f6315349f9d68ab53c39
Eva Pace <eba.pachi@gmail.com>
2022-04-21 18:53:41 -0300

    table: Add filter to 'select'

--- table/select.sh
+++ table/select.sh
@@ -3,6 +3,7 @@
 # Usage:
 # $ ./table/select.sh '.' cars
 # $ ./table/select.sh '.id,.name' cars
+# $ ./table/select.sh '.name' cars '.id==456'
 
 set -e
 
@@ -14,5 +15,11 @@ DATA_DIR="$HOME/.bdg/data"
 ATTRIBUTES=$1
 # eg: cars
 TABLE=$2
+# eg: '.id==456'
+FILTERS=$3
 
-cat "$DATA_DIR/$TABLE" | jq $ATTRIBUTES
+if [ -z "$FILTERS" ]; then
+  cat "$DATA_DIR/$TABLE" | jq $ATTRIBUTES
+else
+  cat "$DATA_DIR/$TABLE" | jq "$ATTRIBUTES | select($FILTERS)"
+fi
